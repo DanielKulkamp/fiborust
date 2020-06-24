2ce14650be7b311aa89e1b585c373c46edffb294
Daniel Clemes Kulkamp <daniel.kulkamp@gmail.com>
2020-06-24 16:48:27 -0300

    working

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,33 @@ use std::io;
 
 fn main() {
     println!("Enter n for the nth Fibonacci number:");
+    let mut nstr = String::new();
+    let mut n : u64;
+    loop {
 
-    let mut n = String::new();
-    io::stdin().read_line(&mut n)
-        .expect("Something went wrong");
-    println!("{}",n);
-        
+        io::stdin().read_line(&mut nstr)
+            .expect("Something went wrong");
+
+        n = match nstr.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a valid positive integer!");
+                nstr = String::new();
+                continue;
+            }
+        };
+        break;
+    };
+    if n < 2 {
+        println!("1");
+    } else {
+        let mut n1 = 1u64;
+        let mut n2 = 1u64;
+        while n>1 {
+            n = n-1;
+            n2 = n2+n1;
+            n1 = n2-n1;                
+        }
+        println!("{}",n2);
+    }
 }
